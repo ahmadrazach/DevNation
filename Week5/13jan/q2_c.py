0e8acacb9b5ee4b6f864136c31016e7d1946bbba
Ahmad Raza <ahmadrazach26@gmail.com>
2022-01-14 19:00:51 +0500

    update q2_c.py
    Done with it!

--- Week5/13jan/q2_c.py
+++ Week5/13jan/q2_c.py
@@ -1,6 +1,24 @@
-# Q2 part c
-row = int(input("PLease enter the rows :"))
-for i in range(row):
-    for j in range(i, row):
-        print("*", end=" ")
+# Q2_c
+# print that !
+# 1010101
+#  10101
+#   101
+#    1
+
+#rows = input("Please enter the no. of rows :")
+rows = 4
+
+for i in range(4):  # reverse hill pattern
+    for j in range(i+1):
+        print(" ", end=" ")
+    for j in range(i, 4-1):
+        if j % 2 == 0:
+            print("1", end=" ")
+        else:
+            print("0", end=" ")
+    for j in range(i, 4):
+        if j % 2 == 0:
+            print("1", end=" ")
+        else:
+            print("0", end=" ")
     print()
