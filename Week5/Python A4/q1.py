0f2c5760556c5ae4cd5e694f920ca60602425a3f
Ahmad Raza <ahmadrazach26@gmail.com>
2022-01-21 03:33:40 +0500

    Create q1.py
    Done with structure of the question

---
+++ Week5/Python A4/q1.py
@@ -0,0 +1,11 @@
+# Collatz Conjecture
+
+
+# take a postive integer input
+number = int(input("Please enter a number : "))
+# if number is odd:
+#  multiply it by 3 add 1 (3n+1)
+# if number is even:
+#  divide it by 2 (n/2)
+
+# print Collatz Chain as well as length of the chain
