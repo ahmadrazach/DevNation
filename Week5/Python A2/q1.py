9b759b6b8b2ecc0879ee93d89378ae24631ac081
Ahmad Raza <ahmadrazach26@gmail.com>
2022-01-20 19:43:00 +0500

    update q1.py
    Done with the 1st question

--- Week5/Python A2/q1.py
+++ Week5/Python A2/q1.py
@@ -1,7 +1,31 @@
 string = input("Enter a string")
-sum = 0
 
 
-def sum(string):
+def ascii_sum(string):  # function of ASCII sum
+    sum = 0
+    for i in string:
+        sum = sum+ord(i)
+        print(i)
+    print(sum)
 
-    print(ord(string))
+
+def ascii_sum_odd(string):  # function of ASCII sum odd
+    sum = 0
+    for i in range(1, len(string)+1):
+        if i % 2 == 1:
+            sum = sum+ord(string[i-1])
+
+    print("Sum of odd numbers : ", sum)
+
+
+def ascii_sum_even(string):  # function of ASCII sum even
+    sum = 0
+    for i in range(1, len(string)+1):
+        if i % 2 == 0:
+            sum = sum+ord(string[i-1])
+
+    print("Sum of even numbers : ", sum)
+
+
+ascii_sum(string)
+ascii_sum_odd(string)
