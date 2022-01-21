0e1551793f80c8f6a7e2209816477f1abffe3690
Ahmad Raza <ahmadrazach26@gmail.com>
2022-01-21 17:59:55 +0500

    Update q3.py

--- Week5/Python A4/q3.py
+++ Week5/Python A4/q3.py
@@ -6,8 +6,9 @@ roll_times = int(input("How many times would like to roll the dice? - "))
 #   for loop to run till the the times
 for i in range(roll_times):
     sum = 0
-#       for loop to run 2 times
+#   for loop to run 2 times
     for j in range(2):
-        no = random.randomint(1, 6)
+        no = random.randint(1, 6)
         sum += no
-    print("Dice roll", i, "sum : ", sum)
+        print(j+1, "st value :", no)
+    print("Dice roll", i+1, "sum : ", sum)
