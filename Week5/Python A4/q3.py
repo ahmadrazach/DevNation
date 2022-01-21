674c6dbf67be02cc5007995bfbb07b5ac0ea6378
Ahmad Raza <ahmadrazach26@gmail.com>
2022-01-21 17:53:57 +0500

    create q3.py

---
+++ Week5/Python A4/q3.py
@@ -0,0 +1,13 @@
+# Lucky 7 in LUDO
+import random
+# ask user how may times the six-sided dice rolled
+roll_times = int(input("How many times would like to roll the dice? - "))
+
+#   for loop to run till the the times
+for i in range(roll_times):
+    sum = 0
+#       for loop to run 2 times
+    for j in range(2):
+        no = random.randomint(1, 6)
+        sum += no
+    print("Dice roll", i, "sum : ", sum)
