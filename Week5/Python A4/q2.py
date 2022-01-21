14189093b770916e478302e5735795a78a6c31e4
Ahmad Raza <ahmadrazach26@gmail.com>
2022-01-21 17:32:52 +0500

    Create q2.py
    Done with structure of the q2

---
+++ Week5/Python A4/q2.py
@@ -0,0 +1,9 @@
+# Nested Loops
+# take input of how long and wide the flag would be
+flag_length = int(input("How long is your flag? - "))
+flag_width = int(input("How wide is your flag? - "))
+# take input of flag pole's length and width
+flagpole_length = int(input("How long is your flagpole? - "))
+flagpole_width = int(input("How wide is your flagpole? - "))
+
+# make a function named rectangle(I,b)
