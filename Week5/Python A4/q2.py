6e65da596f732375ec5326d9cbed5fd00fece22c
Ahmad Raza <ahmadrazach26@gmail.com>
2022-01-21 17:33:12 +0500

    Update q2.py
    Done with q2 solution

--- Week5/Python A4/q2.py
+++ Week5/Python A4/q2.py
@@ -7,3 +7,19 @@ flagpole_length = int(input("How long is your flagpole? - "))
 flagpole_width = int(input("How wide is your flagpole? - "))
 
 # make a function named rectangle(I,b)
+
+
+def rectangle(l, b):
+    #   loop untill the length of rectangle
+    for i in range(l):
+        #       loop untill wide of the rectangle
+        for i in range(b):
+            #           print("*",end="")
+            print("*", end="")
+        print()
+
+
+# give flag length and breadth as arguments to the function
+rectangle(flag_length, flag_width)
+# give flagpole's length and width as arguments to the function
+rectangle(flagpole_length, flagpole_width)
