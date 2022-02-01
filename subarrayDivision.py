8f9e9b80e1b03cb56cf4e4682c2119461f851dd9
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-02 02:40:37 +0500

    create subarrayDivision.py
    Done with the problem's structure of the subarrayDivison

---
+++ subarrayDivision.py
@@ -0,0 +1,29 @@
+s = [2, 2, 1, 3, 2]
+d = 4
+m = 2
+# Returns
+# int: the number of ways the bar can be divided
+
+
+def birthday(s, d, m):
+    # your code
+    # add_square_values=0
+    # contiguous_result=0
+    # loop to the s
+        # loop till m
+            # add_sqaure_values+=s[j]
+
+        # check if add_sqaure values == d:
+
+            # check if i==0:
+                # contiguous_result+=1
+            # check if contiguous_result==add_square values
+                # contiguous_result+=1
+            # else:
+                # pass
+        # else:
+            # pass
+    return(con))
+
+
+print(birthday(s, d, m))
