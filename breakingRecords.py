e9944f4811427918de8a7f56021b03c6133ffb61
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-01 23:30:15 +0500

    Update breakingRecords.py
    Done with python code of it

--- breakingRecords.py
+++ breakingRecords.py
@@ -6,6 +6,18 @@ scores = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42]
 
 
 def breakingRecords(scores):
+    min_points = scores[0]
+    max_points = scores[0]
+    min_count = 0
+    max_count = 0
+    for i in range(1, len(scores)):
+        if scores[i] > max_points:
+            max_points = scores[i]
+            max_count += 1
+        if scores[i] < min_points:
+            min_points = scores[i]
+            min_count += 1
+    return [min_count, max_count]
 
 
 print(breakingRecords(scores))
