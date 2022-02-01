98039ad9c6bbc78a3c3916f9c99140b675beb6ef
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-01 23:28:20 +0500

    create breakingRecords.py
    Done with making strcture and pattern

---
+++ breakingRecords.py
@@ -0,0 +1,11 @@
+scores = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42]
+#  RETURNS
+# int[2]: An array with the numbers of times she broke her records.
+# Index 0 return min points records
+# Index 1 for breaking max points records.
+
+
+def breakingRecords(scores):
+
+
+print(breakingRecords(scores))
