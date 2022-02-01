df89b0f799ec85c836065eec7cc558fb46fdabe5
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-02 00:56:10 +0500

    create divisibleSumPairs.py
    Done with the structure and pattern of the problem

---
+++ divisibleSumpairs.py
@@ -0,0 +1,12 @@
+def divisibleSumPairs(n, k, ar):
+    # Complete this function
+    # no. counting variable
+    # first loop that goes to the n length
+    # second loop that also goes to n
+    # if i<j and ar[i]+ar[j]%k==0:
+    # count the no.
+
+    # return no. of pairs
+
+
+print(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]))
