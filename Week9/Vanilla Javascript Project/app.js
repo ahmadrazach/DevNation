e1a09eefe6fda0ba1341b16e19e4cd2444b2737a
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-06 02:46:31 +0500

    update app.js
    Done with basic styling of app.js

--- Week9/Vanilla Javascript Project/app.js
+++ Week9/Vanilla Javascript Project/app.js
@@ -1,6 +1,32 @@
 //Selectors
-
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 //Event listeners
-
+todoButton.addEventListener("click", addTodo);
 //Functions
+function addTodo(event){
+    //Prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv=document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo=document.createElement('li');
+    newTodo.innerText='hey';
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //Check mark button
+    const completedButton=document.createElement("button");
+    completedButton.innerHTML='<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //check trash button
+    const trashButton=document.createElement("button");
+    trashButton.innerHTML='<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+}
