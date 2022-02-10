22ad31f0bc66479d5341c1201fd07ea6c831d1d0
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-11 01:38:07 +0500

    update in javascript

--- Week10/NotesApp Javascript/script.js
+++ Week10/NotesApp Javascript/script.js
@@ -1,3 +1,19 @@
 /*
+    editbtn d.qS(.edit)
+    noteEl
+        d.qs(.notes)
+    deleteBtn 
+        d.qs('.delete')
+    main
 
 */
+const notesEl=document.querySelector(".notes");
+const editBtn=document.querySelector(".edit");
+const deleteBtn=document.querySelector(".delete");
+const main=notesEl.querySelector(".main");
+const textArea=notesEl.querySelector("textarea");
+
+editBtn.addEventListener("click",()=>{
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+})
